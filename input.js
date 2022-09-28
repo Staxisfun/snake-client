b530eeb97e4ec8b19d57aa214495d07076d664c6
Matt Wilson <mwilson82@hotmail.com>
2022-09-28 23:49:52 +0000

    gave the snake movement ability and completed 'WASD Movement'

--- input.js
+++ input.js
@@ -12,9 +12,23 @@ const setupInput = (conn) => {
 };
 
 const handleUserInput = function (key) {
+  
   if (key === '\u0003') {
     process.exit();
   }
+  if (key === 'w') {
+    connection.write("Move: up");
+  }
+  if (key === 'a') {
+    connection.write("Move: left")
+  }
+  if (key === 'd') {
+    connection.write("Move: right")
+  }
+  if (key === 's') {
+    connection.write("Move: down")
+  }
+
 };
 
 
