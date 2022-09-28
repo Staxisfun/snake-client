6fe2318465a00a0cfb51fa99e84a806c50107a80
Matt Wilson <mwilson82@hotmail.com>
2022-09-28 22:44:37 +0000

    completed 'input module' challenge

--- play.js
+++ play.js
@@ -2,25 +2,13 @@
 
 const {connect} = require("./client")
 
+const { setupInput } = require("./input");
+
+
 console.log("Connecting ...");
+
 connect();
 
 
-// setup interface to handle user input from stdin
+setupInput();
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput); 
-  return stdin;
-};
-
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-
-setupInput()
